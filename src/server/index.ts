bdfbcc11eb3dd415b6ff60e287913108f5b0ee98
Sergio <sergiot.fontal@gmail.com>
2023-02-18 22:04:38 +0100

    Create router

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import morgan from "morgan";
+import robotsRouter from "./routers/robotsRouters.js";
 
 export const app = express();
 app.disable("x-powered-by");
 
 app.use(morgan("dev"));
 app.use(express.json());
+
+app.use("/robots", robotsRouter);
