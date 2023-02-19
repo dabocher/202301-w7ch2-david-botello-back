632f5d7bcec5a9eb2c5576b3e8dea41282f1b6ec
Sergio <sergiot.fontal@gmail.com>
2023-02-19 17:49:16 +0100

    Add new ESLint rule

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,5 @@ import { startServer } from "./server/routers/startServer.js";
 const port = process.env.PORT ?? 4000;
 const mongoDbUrl = process.env.MONGODB_URL!;
 
-// eslint-disable-next-line no-implicit-coercion
 await startServer(+port);
 await connectDatabase(mongoDbUrl);
