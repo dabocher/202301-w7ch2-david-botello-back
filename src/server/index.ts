289955405c815f4256157e95978143d727dd8320
Sergio <sergiot.fontal@gmail.com>
2023-02-19 15:36:48 +0100

    Install Cors and its types

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import morgan from "morgan";
 import robotsRouter from "./routers/robotsRouters.js";
+import cors from "cors";
 
 export const app = express();
 app.disable("x-powered-by");
 
 app.use(morgan("dev"));
+app.use(cors());
 app.use(express.json());
 
 app.use("/robots", robotsRouter);
