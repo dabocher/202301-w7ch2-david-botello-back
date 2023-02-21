2e46b915ac0aaffafc4dfa09e9d335f239d033f8
Sergio <sergiot.fontal@gmail.com>
2023-02-21 14:53:07 +0100

    Add CustomRequest and CustomJwtPayload interfaces

--- src/server/types.ts
+++ src/server/types.ts
@@ -1,4 +1,15 @@
+import { type Request } from "express";
+import { type JwtPayload } from "jsonwebtoken";
+
 export interface UserCredentials {
   username: string;
   password: string;
+}
+
+export interface CustomRequest extends Request {
+  userId: string;
+}
+
+export interface CustomJwtPayload extends JwtPayload {
+  sub: string;
 }
